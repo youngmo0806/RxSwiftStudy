1ce9300bb835972903dfd483c694ec48e28ce2ce
youngmo0806 <youngmo0806@naver.com>
2020-11-22 04:13:30 +0900

    common.swift

--- RxSwiftStudy/Common.swift
+++ RxSwiftStudy/Common.swift
@@ -7,6 +7,24 @@
 
 import UIKit
 
-class Common: NSObject {
+let largeImageUrl = "https://picsum.photos/1024/768/?random"
+let largerImageUrl = "https://picsum.photos/1280/720/?random"
+let largestImageUrl = "https://picsum.photos/2560/1440/?random"
 
+func asyncLoadImage(from imageUrl: String, completed: @escaping (UIImage?) -> void) {
+    
+    DispatchQueue.global().asyc{
+        guard let url = URL(string: imageUrl) else {
+            completed(nil)
+            return
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            completed(nil)
+            return
+        }
+            
+        let image = UIImage(data: data)
+        completed(image)
+    }
 }
